898061a2bd882b06f6b325284082d1ba0cdd2cde
ZIBMBRAZIL <moreira_igor9@hotmail.com>
2022-09-16 02:14:56 -0300

    CRUD Products

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,11 @@
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 
@@ -9,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
