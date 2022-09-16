5e35edeaef28ef1bc45867122d94ca1aadaf2cba
ZIBMBRAZIL <moreira_igor9@hotmail.com>
2022-09-15 23:10:40 -0300

    Tratamento de erros

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
@@ -9,6 +10,22 @@ app.use(express.json());
 
 app.use(routes);
 
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
+
 app.listen(3333, () => {
+  // eslint-disable-next-line no-console
   console.log('🟢 SERVER ON 🟢 -> Server start on port 3333! ');
 });
