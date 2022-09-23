24876ffd837f6a1e4dfe44dfc810d544f1d5a1bd
ZIBMBRAZIL <moreira_igor9@hotmail.com>
2022-09-22 22:34:05 -0300

    Middleware Rate Limiter

---
+++ src/shared/http/middlewares/rateLimiter.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from 'express';
+import redis from 'redis';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+import AppError from '@shared/errors/AppError';
+
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    const redisClient = redis.createClient({
+      legacyMode: true,
+      password: process.env.REDIS_PASS || undefined,
+      socket: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
+      },
+    });
+
+    const limiter = new RateLimiterRedis({
+      storeClient: redisClient,
+      keyPrefix: 'ratelimit',
+      points: 5,
+      duration: 1,
+    });
+
+    await limiter.consume(request.ip);
+
+    return next();
+  } catch (err) {
+    throw new AppError('Too many requests.', 429);
+  }
+}
